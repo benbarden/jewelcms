d83946b96e5cbaa4ea0290bfc2958ac288c6e718
Ben Barden <github@benbarden.com>
2015-02-22 13:30:33 +0000

    Fixing unit test bootstrap

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -10,5 +10,8 @@ require dirname(__FILE__).'/../upload/lib/Cms/Core/Constants/System.php';
 // inj Framework v3: Autoloader
 require ABS_ROOT.'/lib/Cms/Core/Autoloader/Base.php';
 
+// Vendor autoloader
+require_once ABS_ROOT.'vendor/autoload.php';
+
 // Test Framework
 require dirname(__FILE__).'/lib/Test/Cms/Di/ContainerBase.php';
